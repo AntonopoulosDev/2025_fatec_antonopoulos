910e4a43bc804fc8c75762a333c9306d3cb1dd31
Sócrates Antonopoulos <antonopoulos2301@gmail.com>
2025-02-21 21:41:07 -0300

    adicionado teste subtração

--- teste_calculadora.py
+++ teste_calculadora.py
@@ -2,4 +2,9 @@ import calculadora
 
 a = 3
 b = 2
-print(f'{a} + {b} = {calculadora.calculadora(a, b)}')
+print(f'{a} + {b} = {calculadora.calculadora(a, b, "adição")}')
+print(f'{a} - {b} = {calculadora.calculadora(a, b, "subtração")}')
+
+# minor não trás novas funcionalidades, apenas correções de bugs
+# patch trás correções de bugs e pequenas melhorias
+# major trás novas funcionalidades e mudanças que podem quebrar o código    
